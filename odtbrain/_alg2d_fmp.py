cf70d66bf27e371d3ecb2bc0a037f2e3efc7ad91
Paul Müller <dev@craban.de>
2023-05-11 15:19:08 +0200

    cleanup

--- odtbrain/_alg2d_fmp.py
+++ odtbrain/_alg2d_fmp.py
@@ -253,7 +253,6 @@ def fourier_map_2d(uSin, angles, res, nm, lD=0, semi_coverage=False,
     # Filter data
     kinx, kiny = np.meshgrid(np.fft.fftshift(kx), np.fft.fftshift(kx))
     Fcomp[np.where((kinx**2 + kiny**2) > 2 * km**2)] = 0
-    # Fcomp[np.where(kinx**2+kiny**2<km)] = 0
 
     # Fcomp is centered at K = 0 due to the way we chose kintp/coords
     f = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(Fcomp)))
